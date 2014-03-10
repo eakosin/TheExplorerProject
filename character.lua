7306a0a9c5826c31df37ef8c629f77d46a063f9b
dmluu <dmluu@g.cofc.edu>
2014-03-10 18:34:26 -0400

    added comments to character.lua

--- character.lua
+++ character.lua
@@ -10,28 +10,58 @@ character.imageName = "crapcharacter.png"
 character.x, character.y = 0,0
 character.dn, character.ds, character.dw, character.de = 0,0,0,0
 character.canMove = {north = true, south = true, west = true, east = true}
-character.stats = {health = 100, energy = 50, weapon = 5, armor = 5, status = {}} 
+character.stats = {health = 100, energy = 50, weapon = 5, armor = 5, status = {}}
 
+
+--[[
+character:new(world)
+This function pass the world object.
+]]--
+--param: world
+--return: new
 function character:new(world)
 	new = {}
 	setmetatable(new, self)
 	self.__index = self
 	new.world = world
 	new.canMove = {north = true, south = true, west = true, east = true}
-	new.stats = {health = 100, energy = 50, weapon = 5, armor = 5, status = {}} 
+	new.stats = {health = 100, energy = 50, weapon = 5, armor = 5, status = {}}
 	return new
 end
 
+
+--[[
+character:initialize(isPlayer)
+This function initialize the player.
+]]--
+--param: isPlayer
+--return: none
 function character:initialize(isPlayer)
 	self.isPlayer = isPlayer
 	self.image = love.graphics.newImage("images/"..self.imageName)
 end
 
+
+--[[
+character:placeCharacter(x, y)
+This function places character on the grid.
+]]--
+--param: x
+--param: y
+--return: none
 function character:placeCharacter(x, y)
 	self.x = x
 	self.y = y
 end
 
+
+
+--[[
+character:fillEventQueue()
+This function...
+]]--
+--param: none
+--return: none
 function character:fillEventQueue()
 	if(self.world.keyState.up) then
 		self.dn = -8
@@ -60,6 +90,20 @@ function character:fillEventQueue()
 	end
 end
 
+
+
+function character:processEvent()
+
+end
+
+
+
+--[[
+character:processChanges()
+This function
+]]--
+--param: none
+--return: none
 function character:processChanges()
 	if(self.canMove.north) then
 		self.y = self.y + self.dn
@@ -79,6 +123,12 @@ function character:processChanges()
 	self.canMove.east = true
 end
 
+--[[
+character:draw()
+
+]]--
+--param: none
+--return: none
 function character:draw()
 	love.graphics.draw( self.image, self.x, self.y, 0, 1, 1, 8, 32, 0, 0 )
 end
