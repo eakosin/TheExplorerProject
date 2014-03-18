6e42357ce9ef2a9978b94eddcc7630fd2d505742
rgb <rgb@rgb-X202E.(none)>
2014-03-18 07:09:49 -0400

    removed button stuff from progress bar to make it a stub

--- ui.lua
+++ ui.lua
@@ -77,18 +77,6 @@ function progressBar:configure(parameters)
 	for key, value in pairs(parameters) do
 		self[key] = value
 	end
-	self.font = love.graphics.newFont("fonts/Karla/Karla-Regular.ttf", 18)
-	self.backImage = love.graphics.newImage("images/"..self.backImageName)
-	self.canvas = love.graphics.newCanvas(self.width, self.height)
-	local buttonQuad = love.graphics.newQuad(0, 0, self.width, self.height, self.backImage:getDimensions())
-	love.graphics.setCanvas(self.canvas)
-	love.graphics.setBackgroundColor(0,0,0,0)
-	love.graphics.clear()
-	love.graphics.draw(self.backImage, buttonQuad)
-	love.graphics.setFont(self.font)
-	love.graphics.setColor(255, 255, 255, 255)
-	love.graphics.print(self.text, self.textOffset, ((self.height / 2) - (self.font:getHeight() / 2) - 2))
-	love.graphics.setCanvas()
 end
 
 --[[
