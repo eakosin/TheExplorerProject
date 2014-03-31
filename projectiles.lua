f153a7d276252ece1ed0c4ea8a76b303bd3c782f
jarcarroll <jacarrol@g.cofc.edu>
2014-03-31 18:20:47 -0400

    Projectile and CrapStalagmite
    The projectile class was modified to include collision detection, speed
    and movement, and location.
    The crapstalagmite.png image was made to represent a basic projectile.

--- projectiles.lua
+++ projectiles.lua
@@ -9,8 +9,8 @@ projectile.imageName = "crapprojectile.png"
 projectile.x, projectile.y = 0,0
 projectile.dx, projectile.dy = 0,0
 projectile.destx, projectile.desty = 0,0
-projectile.speed = 0
-projectile.path = {}
+projectile.speedScaler = {x = 0, y=0}
+projectile.path = {pathType="linear",speed="3"}
 projectile.canMove = true
 projectile.stats = {class = "", energy = 0, damage = 5, defense = 5, status = {}}
 
@@ -23,28 +23,48 @@ function projectile:new(world)
 	return new
 end
 function projectile:initialize(destx, desty, speed, path)
-	--self.image = love.graphics.newImage("images/"..self.imageName)
+	self.image = love.graphics.newImage("images/"..self.imageName)
 	self.destx = destx or self.destx
 	self.desty = desty or self.destx
 	self.speed = speed or self.speed
-	self.path = path
+	self.path = path or self.path
+	self.speedScaler.x = ((self.destx - self.x)/(self.desty - self.y))
+	self.speedScaler.y = 1 - ((self.destx - self.x)/(self.desty - self.y))
 end
 function projectile:placeProjectile(x, y)
-	self.x = x
-	self.y = y
+	self.x = self.x + self.dx
+	self.y = self.y + self.dy
 end
 function projectile:fillEventQueue()
 	--process curve, calc dx and dy based off of speed/path
+	self.dx = self.speedScaler.x * self.path.speed
+	self.dy = self.speedScaler.y * self.path.speed
+	self.world.eventQueue.world[#self.world.eventQueue.world+1]={name="collision", object=self}
+	--self.level
 end
 function projectile:processEvent(event)
-	--change or modify projectile based off of user action and projectile calss
+	--change or modify projectile based off of user action and projectile class
+	width = event.object.image:getWidth()
+	height = event.object.image:getHeight()
+	topLeftCorner = {x = event.object.x, y = event.object.y}
+	bottomRightCorner = {x = event.object.x + event.object.image:getWidth(), y = event.object.y + event.object.image:getHeight()}
+	if ((self.x >= topLeftCorner.x and self.y >= topLeftCorner.y) and 
+		(self.x <= bottomRightCorner.x and self.y <= bottomRightCorner.y)) then
+		self.canMove = false
+	end
+	if ((event.object.x >= self.x and event.object.y >= self.y) and 
+		(event.object.x <= self.x + self.image:getWidth() and event.object.y <= self.y + self.image:getHeight())) then
+		self.canMove = false
+	end
 end
 function projectile:processChanges()
 	if (self.canMove == false)
 		then
-		--dissapear, destroy, sick, etc.
+		self.world.eventQueue.world[#self.world.eventQueue.world+1]={name="destroyprojectile", object=self}
 	else
 		--cont. moving based of of changes in fill event queue
+		self.x = self.x + self.dx
+		self.y = self.y + self.dy
 	end
 end
 function projectile:draw()
